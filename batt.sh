7e2ccc9e742a68656be86115a17be4bd49f9648e
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-03-24 13:46:41 -0500

    Fix problems with grep

--- batt.sh
+++ batt.sh
@@ -11,14 +11,14 @@ alertSoundLinux=/usr/share/sounds/common/battery-low.ogg
 
 while true; do
     if [[ "$(uname)" == "Darwin" ]]; then
-        if pmset -g batt | head -n 1 | grep -q "Battery" && [ $(pmset -g batt | grep -o '[0-9]\{1,3\}%' | tr -d '%') -le $batteryPercentAlert ]; then
+        if pmset -g batt | head -n 1 | grep -i -q "Battery" && [ $(pmset -g batt | grep -i -o '[0-9]\{1,3\}%' | tr -d '%') -le $batteryPercentAlert ]; then
             afplay $alertSoundMac
             sleep $timeInbetweenAlerts
         fi
     else
-        batteryInfo=$(upower -i $(upower -e | grep BAT))
-        batteryPercent=$(echo "$batteryInfo" | grep -oP 'percentage:\s+\K\d+')
-        chargingState=$(echo "$batteryInfo" | grep -oP 'state:\s+\K\w+')
+        batteryInfo=$(upower -i $(upower -e | grep -i BAT))
+        batteryPercent=$(echo "$batteryInfo" | grep -i -o -P 'percentage:\s+\K\d+')
+        chargingState=$(echo "$batteryInfo" | grep -i -o -P 'state:\s+\K\w+')
 
         if [[ "$chargingState" == "discharging" && $batteryPercent -le $batteryPercentAlert ]]; then
             paplay $alertSoundLinux
