bd0389702484dc26557fca64998a71d80063c13d
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-03-31 20:51:12 -0500

    optimize to only call uname once as it shouldnt change

--- batt.sh
+++ batt.sh
@@ -6,11 +6,15 @@ batteryPercentAlert=20
 alertSoundMac=/System/Library/Sounds/Funk.aiff
 alertSoundLinux=/usr/share/sounds/common/battery-low.ogg
 
+# If you need to override system type (Ex. system doesnt have "uname" available)
+# You can manually override this to "Darwin" for mac or "Linux" for Lipnux
+systemType=$(uname)
+
 # This is from https://github.com/NanashiTheNameless/BatteryAlertTool
 # It is licensed under https://github.com/NanashiTheNameless/BatteryAlertTool/blob/main/license.md
 
 while true; do
-    if [[ "$(uname)" == "Darwin" ]]; then
+    if [[ "$systemType" == "Darwin" ]]; then
         if pmset -g batt | head -n 1 | grep -i -q "Battery" && [ $(pmset -g batt | grep -i -o '[0-9]\{1,3\}%' | tr -d '%') -le $batteryPercentAlert ]; then
             afplay $alertSoundMac
             sleep $timeInbetweenAlerts
